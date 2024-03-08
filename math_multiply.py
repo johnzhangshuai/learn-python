9a479b01d8f90fc99873c3b7a88cc25692d96995
John Zhang <150347375+johnzhangshuai@users.noreply.github.com>
2024-03-08 15:28:32 -0800

    """math calculator

---
+++ math_multiply.py
@@ -0,0 +1,20 @@
+"""this is an example of maths operations
+Operators	Operation	Example
+**	Exponent	2 ** 3 = 8
+%	Modulus/Remainder	22 % 8 = 6
+//	Integer division	22 // 8 = 2
+/	Division	22 / 8 = 2.75
+*	Multiplication	3 * 3 = 9
+-	Subtraction	5 - 2 = 3
++	Addition	2 + 2 = 4
+
+"""
+
+print("multiply two numbers")
+multiplierStr = input("Please enter the multiplier : ")
+multiplier = int(multiplierStr)
+multiplicand = int(input("Please enter the multiplicand"))
+product = multiplier * multiplicand
+print("the product =", product)
+
+print("Divide a number")
